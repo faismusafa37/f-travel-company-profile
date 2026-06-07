4189297673259ad7c9b2f9db8768f41e1422c14c
COCOROBO 開発者 <r_firdaus@cocorobo.jp>
2026-06-07 19:38:15 +0700

    update login button

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client"],
   experimental: {
+    cpus: 1,
     serverActions: {
       bodySizeLimit: "50mb",
     },
